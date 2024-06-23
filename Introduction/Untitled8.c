42a707a86d3e61fe6aabb923799ef5e073c8bf50
Soyayeb Hasan Shafin <sa.fine@outlook.com>
2024-06-23 11:50:06 +0600

    intro up to 9

---
+++ Introduction/Untitled8.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main(){
+    int firstValue,secondValue,thirdValue;
+    scanf("%d %d %d",&firstValue,&secondValue,&thirdValue);
+    printf("First value= %d, Last Value= %d",firstValue,thirdValue);
+}
---
+++ Introduction/Untitled9.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include <stdbool.h>
+void main(){
+    double a;
+    bool b;
+    a=3.15435;
+    b=true;
+    printf("The double value: %f\nThe boolean value: %d",a,b);
+}
